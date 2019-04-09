20e0e3db089d135905ee0437adb19817c47d7d4c
hoa44 <hoa44@njit.edu>
2019-04-09 12:50:12 -0400

    Created 5 tests with 6 assertions.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    // Grabs a random user from teh DB and verifies the ID is an integer and that he user column is a string.
+    public function testUserID()
+
+    {
+        $user = User::inRandomOrder()->first();
+
+        //dd($user);
+
+        $this->assertIsInt($user->id);
+
+        //dd($user->id);
+
+        $this->assertIsString($user->name);
+
+        //dd($user->name);
+
+
+        //$this->assertTrue(true);
+    }
+
+
+}
